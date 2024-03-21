c94f52c7c97d874825eb646afdb5915517479ec3
Wenxuan Qiu <1737739958@qq.com>
2024-03-21 14:37:45 +0800

    Initial SpringBoot Study

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @Author Wenxuan.qiu
+ * @Date 2024/3/21 14:36
+ * @Description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
