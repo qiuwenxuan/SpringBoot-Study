417dfded9de5913ca2527aab3c04b115e829c91a
Wenxuan.qiu <1737739958@qq.com>
2024-03-21 22:46:36 +0800

    initial maven project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @Author Wenxuan.qiu
+ * @Date 2024/3/21 22:44
+ * @Description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
